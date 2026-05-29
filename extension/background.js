798d27a5b52cae43fe1d5b97548b0337ae57b7ca
sailajapinisetty <sailaja.pinisetty@slalom.com>
2026-05-29 15:30:00 -0400

    new tab

--- extension/background.js
+++ extension/background.js
@@ -3,3 +3,12 @@
 self.addEventListener('install', () => {
   self.skipWaiting();
 });
+
+// Open extension UI in a new browser tab when the action icon is clicked
+if (typeof chrome !== 'undefined' && chrome.action && chrome.action.onClicked) {
+  chrome.action.onClicked.addListener(() => {
+    const url = chrome.runtime.getURL('popup.html');
+    // open as a regular tab in the current window
+    chrome.tabs.create({ url });
+  });
+}
